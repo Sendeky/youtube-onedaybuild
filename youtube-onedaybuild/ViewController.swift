b8a742be623a49234e7b001dbf3e78ba5ff103f0
Ruslan Spirkin <Ruslan1@Ruslans-MacBook-Air.local>
2022-04-04 11:33:46 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Ruslan Spirkin on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
