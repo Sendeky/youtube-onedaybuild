3bb6f0911ddd56008fd979c43fa5732b10aec5fa
Ruslan Spirkin <Ruslan1@Ruslans-MacBook-Air.local>
2022-04-05 19:26:01 -0700

    ConstantsTemplate.swift is now being used instead of Constants.swift

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
+
+// Uncomment the code and use your own API_KEY, API_URL, and PLAYLIST_ID
+
+/*
 struct Constants {
     
-    static var API_KEY = "AIzaSyCpui6EiMTEcoYIupXOSPYJSgWY-JRPB2s"
-    static var PLAYLIST_ID = "UUBJycsmduvYEL83R_U4JriQ"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
+*/
